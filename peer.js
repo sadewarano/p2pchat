91976ad026e0eac95f2507ea4da5d23a0d9cfdd4
sadewarano <sadewarano@gmail.com>
2026-07-18 00:26:25 +0700

    Update peer.js

--- peer.js
+++ peer.js
@@ -1,112 +1,166 @@
-const peers = new Map();
+const peers=new Map();
 
-const cfg = {
-  iceServers:[
-    {urls:"stun:stun.l.google.com:19302"},
-    {urls:"stun:stun1.l.google.com:19302"}
-  ]
+const cfg={
+ iceServers:[
+  {urls:"stun:stun.l.google.com:19302"},
+  {urls:"stun:stun1.l.google.com:19302"}
+ ]
 };
 
-const makeId = () =>
-  "p_" + Math.random().toString(36).slice(2,8);
+
+const makeId=()=>
+"p_"+Math.random().toString(36).slice(2,8);
 
 
 function createPeer(id){
-  const pc = new RTCPeerConnection(cfg);
 
-  const peer = {
-    pc,
-    dc:null
-  };
+ const pc=new RTCPeerConnection(cfg);
 
-  peers.set(id,peer);
+ const peer={
+  pc,
+  dc:null,
+  code:null
+ };
 
-  pc.onicecandidate = () => {
-    if(pc.localDescription)
-      peer.code = JSON.stringify({
-        id,
-        sdp:pc.localDescription
-      });
-  };
+ peers.set(id,peer);
 
-  return peer;
+
+ pc.onicecandidate=e=>{
+
+  if(!e.candidate && pc.localDescription){
+
+   peer.code=JSON.stringify({
+    id,
+    sdp:pc.localDescription
+   });
+
+  }
+
+ };
+
+
+ return peer;
 }
 
 
 function openChannel(id,dc){
-  const p = peers.get(id);
-  p.dc = dc;
 
-  dc.onopen = () =>
-    peerOpen(id);
+ const p=peers.get(id);
 
-  dc.onmessage = e =>
-    peerMessage(id,e.data);
+ p.dc=dc;
 
-  dc.onclose = () =>{
-    peers.delete(id);
-    peerClose(id);
-  };
+
+ dc.onopen=()=>{
+  if(window.peerOpen)
+   peerOpen(id);
+ };
+
+
+ dc.onmessage=e=>{
+  if(window.peerMessage)
+   peerMessage(id,e.data);
+ };
+
+
+ dc.onclose=()=>{
+
+  peers.delete(id);
+
+  if(window.peerClose)
+   peerClose(id);
+
+ };
+
 }
+
+
+function waitCode(p){
+
+ return new Promise(r=>{
+
+  let t=setInterval(()=>{
+
+   if(p.code){
+
+    clearInterval(t);
+    r(p.code);
+
+   }
+
+  },50);
+
+ });
+
+}
+
 
 
 async function makeOffer(){
 
-  const id = makeId();
-  const p = createPeer(id);
+ const id=makeId();
 
-  openChannel(
-    id,
-    p.pc.createDataChannel("chat")
-  );
+ const p=createPeer(id);
 
-  await p.pc.setLocalDescription(
-    await p.pc.createOffer()
-  );
 
-  return p.code;
+ openChannel(
+  id,
+  p.pc.createDataChannel("chat")
+ );
+
+
+ await p.pc.setLocalDescription(
+  await p.pc.createOffer()
+ );
+
+
+ return await waitCode(p);
+
 }
+
 
 
 async function makeAnswer(code){
 
-  const {id,sdp}=JSON.parse(code);
+ const {id,sdp}=JSON.parse(code);
 
-  let p = peers.get(id);
-
-  if(!p){
-
-    p=createPeer(id);
-
-    p.pc.ondatachannel=e=>
-      openChannel(id,e.channel);
-  }
+ let p=peers.get(id);
 
 
-  if(sdp.type=="offer"){
+ if(!p){
 
-    await p.pc.setRemoteDescription(sdp);
+  p=createPeer(id);
 
-    await p.pc.setLocalDescription(
-      await p.pc.createAnswer()
-    );
+  p.pc.ondatachannel=e=>{
+   openChannel(id,e.channel);
+  };
 
-    return p.code;
+ }
 
-  }else{
 
-    await p.pc.setRemoteDescription(sdp);
+ await p.pc.setRemoteDescription(sdp);
 
-  }
+
+ if(sdp.type=="offer"){
+
+  await p.pc.setLocalDescription(
+   await p.pc.createAnswer()
+  );
+
+  return await waitCode(p);
+
+ }
+
 }
+
 
 
 function sendPeer(id,text){
 
-  const p=peers.get(id);
+ const p=peers.get(id);
 
-  if(
-    p?.dc?.readyState=="open"
-  )
-    p.dc.send(text);
+ if(
+  p?.dc?.readyState=="open"
+ )
+ p.dc.send(text);
+
 }
